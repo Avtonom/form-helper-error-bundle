8f2b5ec29922294f116d61b3b2fcf7da087a984b
Anton U <avtonomspb@gmail.com>
2019-06-06 19:48:08 +0300

    add FormErrorHelperTrait

---
+++ Trait/FormErrorHelperTrait.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace EShop\TariffBundle\Entity;
+
+use FormHelper\ErrorBundle\Helper\FormErrorHelper;
+
+trait FormErrorHelperTrait
+{
+    /**
+     * @var FormErrorHelper
+     */
+    protected $formErrorHelper;
+
+    /**
+     * @return FormErrorHelper
+     */
+    public function getFormErrorHelper()
+    {
+        return $this->formErrorHelper;
+    }
+
+    /**
+     * @param FormErrorHelper $formErrorHelper
+     */
+    public function setFormErrorHelper(FormErrorHelper $formErrorHelper)
+    {
+        $this->formErrorHelper = $formErrorHelper;
+    }
+}
